93fe1b6f792791069699c6f7fb97243ac3c40f1c
nuclearzzet <nikettjohn@outlook.com>
2024-05-28 20:48:45 +0530

    ✨ HTTP Server Code - 3 (Parser - req, Builder - res)

--- src/http/http.cpp
+++ src/http/http.cpp
@@ -8,7 +8,6 @@ namespace WebCpp
                                                               hs_port{port},
                                                               hs_addr{addr},
                                                               hs_response{""},
-                                                              hs_request{""},
                                                               hs_server{} {};
 
     int HttpServer::initServer()
@@ -48,44 +47,91 @@ namespace WebCpp
 
         while ((this->hs_connSock = accept(hs_lstnSock, NULL, NULL)) != SOCKET_ERROR)
         {
-            std::thread th(&clientHandler, this);
+            std::thread th(&httpClientHandler, this);
             th.detach();
         }
         return 0;
     };
 
-    int HttpServer::clientHandler()
+    int HttpServer::httpClientHandler()
     {
-        constexpr size_t currentSize = 4096;
-        this->hs_request.resize(currentSize);
+        HttpRequest req(this->hs_connSock);
+        HttpResponse res(this->hs_connSock);
+        URL url;
+        Body bod;
 
-        std::istringstream request{};
+        req.read();
+        req.parseRequest(url, bod);
 
-        recv(this->hs_connSock, this->hs_request.data(), currentSize, 0);
-
-        std::cout << this->hs_request << std::endl;
-
-        std::string msg = "<h1>Hello,World!</h1> \n <h2>How do you do?</h2> \n <h3>I am doing fine.</h3>";
-        buildResponse(msg);
-        if (send(this->hs_connSock, this->hs_response.data(), this->hs_response.size(), 0) == this->hs_response.size())
-        {
-            std::cout << this->hs_response << std::endl;
-        }
+        std::string body{"<h1>Hello,World!</h1> \n <h2>How do you do?</h2> \n <h3>I am doing fine.</h3>"};
+        res.buildBody(body);
+        res.sendHeaders();
+        res.flush();
 
         return 0;
     }
 
-    int HttpServer::buildResponse(std::string &msg)
-    {
-        std::ostringstream response{};
-        response << "HTTP/1.1 200 OK\r\n";
-        response << "Content-Type: text/html\r\n";
-        response << "Content-Length: " << msg.length() << "\r\n";
-        response << "\r\n";
-        response << msg << "\r\n\r\n";
+    HttpServer::HttpRequest::HttpRequest(SOCKET sen) : sender{sen} {}
 
-        this->hs_response = response.str();
-        return 0;
+    void HttpServer::HttpRequest::read() noexcept
+    {
+        this->req_raw.resize(4096);
+        recv(this->sender, this->req_raw.data(), 4096, 0);
+    }
+
+    void HttpServer::HttpRequest::parseRequest(URL &u, Body &b) noexcept
+    {
+        std::istringstream st_rLine{this->req_raw.substr(0, this->req_raw.find('\r'))};
+        std::istringstream st_headers{this->req_raw.substr(this->req_raw.find('\n'), (this->req_raw.find_last_of('\r') - 2))};
+        std::string word{};
+
+        std::getline(st_rLine, word, ' ');
+
+        this->req_requestMethod = (word == "GET")     ? Methods::Get
+                                  : (word == "PUT")   ? Methods::Put
+                                  : (word == "POST")  ? Methods::Post
+                                  : (word == "PATCH") ? Methods::Patch
+                                                      : Methods::Delete;
+
+        std::getline(st_rLine, word, ' ');
+        u.path = word;
+        word.clear();
+
+        while (std::getline(st_headers, word))
+            this->req_headers[word.substr(0, (word.find(':') + 1))] = word.substr((word.find(':') + 1), word.find(word.back()));
+
+        for (auto &ref : this->req_headers)
+            std::cout << ref.first << ref.second << std::endl;
+    }
+
+    HttpServer::HttpResponse::HttpResponse(SOCKET rec) : reciever{rec} {}
+
+    void HttpServer::HttpResponse::buildBody(std::string &b)
+    {
+        this->res_body += b;
+    }
+
+    void HttpServer::HttpResponse::flush() noexcept
+    {
+        if (send(this->reciever, this->res_body.data(), this->res_body.size(), 0) != this->res_body.size())
+            std::cout << "Flush failed" << std::endl;
+
+        this->res_body.clear();
+    }
+
+    void HttpServer::HttpResponse::sendHeaders() noexcept
+    {
+        std::ostringstream head{};
+        head << "HTTP/1.1";
+        head << this->res_statusCode << " OK" << "\r\n";
+        for (auto &ref : this->res_headers)
+        {
+            head << ref.first << ": " << ref.second;
+        }
+        head << "\r\n\r\n";
+
+        if (!(send(this->reciever, head.str().data(), head.str().size(), 0) == head.str().size()))
+            std::cout << "Header send failed" << std::endl;
     }
 
     HttpServer::~HttpServer()
--- src/http/http.hpp
+++ src/http/http.hpp
@@ -4,14 +4,20 @@
 
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+
+#include <thread>
+
+#include <vector>
+#include <unordered_map>
+
 #include <iostream>
 #include <sstream>
-#include <thread>
 #include <string>
 #include <string_view>
 
 namespace WebCpp
 {
+
     class HttpServer
     {
     private:
@@ -19,9 +25,67 @@ namespace WebCpp
         SOCKET hs_lstnSock, hs_connSock;
         int hs_port;             // the port on which the server runs
         std::string hs_addr;     // the addr of the server
-        std::string hs_request;  // the request from client
         std::string hs_response; // the response from client
         SOCKADDR_IN hs_server;   // the server struct consisted of communication types
+
+        // The HTTP request methods
+        enum struct Methods : char
+        {
+            Get,
+            Post,
+            Put,
+            Patch,
+            Delete,
+        };
+
+        // the body of the request
+        struct Body
+        {
+        private:
+            std::vector<byte> raw;
+
+        public:
+            std::string toString();
+            std::vector<byte> getRaw();
+        };
+
+        // the target UTI of request and query
+        struct URL
+        {
+            std::string path;
+            std::unordered_map<std::string, std::string> query;
+            std::string toString();
+        };
+
+        // HttpRequest structure
+        struct HttpRequest
+        {
+        private:
+            std::string req_raw;                                      // the raw request from client
+            Methods req_requestMethod;                                // the request method
+            std::unordered_map<std::string, std::string> req_headers; // the headers of the request
+            SOCKET sender;                                            // the request sender
+
+        public:
+            HttpRequest(SOCKET sen);
+            std::string getHeaders() noexcept;
+            void parseRequest(URL &u, Body &b) noexcept;
+            void read() noexcept;
+        };
+
+        struct HttpResponse
+        {
+            HttpResponse(SOCKET rec);
+            void sendHeaders() noexcept;
+            void buildBody(std::string &b);
+            void flush() noexcept;
+
+        private:
+            int res_statusCode;
+            std::unordered_map<std::string, std::string> res_headers;
+            std::string res_body;
+            SOCKET reciever;
+        };
 
     public:
         HttpServer(int port, std::string_view addr);
@@ -29,8 +93,9 @@ namespace WebCpp
         ~HttpServer();
 
     private:
-        int buildResponse(std::string &msg);
-        int clientHandler();
+        int buildHttpResponse(HttpResponse &res);
+        int httpClientHandler();
+        int parseHttpRequest(HttpRequest &req);
     };
 }
 
